3ce3728f9e258cf08d8fc1a75e0ddb39629efd35
VibhashDwivedi <vibhashdwi1005@gmail.com>
2023-10-04 20:54:11 +0530

    mobile view

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <Link className="navbar-brand fw-bolder" to='/home' style={{color:'whitesmoke', fontFamily:'Montserrat'}}>
       <span style={{fontSize:'30px'}}>E</span>pisode <span style={{fontSize:'30px'}} >R</span>andomizer
     </Link>
-    <button
+    {/* <button
       className="navbar-toggler"
       type="button"
       data-bs-toggle="collapse"
@@ -19,7 +19,7 @@ const Navbar = () => {
       aria-label="Toggle navigation"
     >
       <span className="navbar-toggler-icon" />
-    </button>
+    </button> */}
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         
