b6ad05ef08347664cf0edbbf59a8b948bc887b61
VibhashDwivedi <vibhashdwi1005@gmail.com>
2024-09-10 11:06:19 +0530

    new

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import random
 import requests
 import threading
@@ -6,13 +7,39 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": ["*"]}})
 
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.route('/getepisode')
+def get_episode():
+    data = pd.read_csv('friends.csv', encoding='cp1252')
+    Generator = data.iloc[[random.randint(1, 236)], [1, 2, 3, 4, 5, 6, 7]]
+    x = Generator.to_json()
+    return x
+
+@app.route('/getepisodehimym')
+def get_episode_himym():
+    data = pd.read_csv('himym.csv', encoding='cp1252')
+    Generator = data.iloc[[random.randint(1, 208)], [1, 2, 3, 4, 5, 6, 7]]
+    x = Generator.to_json()
+    return x
+
+@app.route('/getepisodetbbt')
+def get_episode_tbbt():
+    data = pd.read_csv('tbbt.csv', encoding='cp1252')
+    Generator = data.iloc[[random.randint(1, 250)], [1, 2, 3, 4, 5, 6, 7]]
+    x = Generator.to_json()
+    return x
+
+@app.route('/getepisodemodernfamily')
 def get_episode_modern():
     data = pd.read_csv('modern_family.csv', encoding='cp1252')
     Generator = data.iloc[[random.randint(1, 250)], [1, 2, 3, 4, 5, 6, 7]]
     x = Generator.to_json()
-    return x 
+    return x
 
 @app.route('/getepisodetheoffice')
 def get_episode_office():
@@ -28,9 +55,30 @@ def get_episode_brooklyn():
     x = Generator.to_json()
     return x
 
+@app.route('/getepisodegot')
+def get_episode_got():
+    data = pd.read_csv('got.csv', encoding='cp1252')
+    Generator = data.iloc[[random.randint(1, 73)], [1, 2, 3, 4, 5, 6, 7]]
+    x = Generator.to_json()
+    return x
+
+@app.route('/getepisodesimpsons')
+def get_episode_simpson():
+    data = pd.read_csv('simpsons.csv', encoding='cp1252')
+    Generator = data.iloc[[random.randint(1, 110)], [1, 2, 3, 4, 5, 6, 7]]
+    x = Generator.to_json()
+    return x
+
+@app.route('/getepisodecommunity')
+def get_episode_community():
+    data = pd.read_csv('community.csv', encoding='cp1252')
+    Generator = data.iloc[[random.randint(1, 110)], [1, 2, 3, 4, 5, 6, 7]]
+    x = Generator.to_json()
+    return x
+
 # Function to make the HTTP GET request
 def reload_website():
-    url = 'https://episode-randomizer-8ij4.onrender.com'
+    url = 'https://yourappname123.onrender.com/getepisodetmkoc'
     try:
         response = requests.get(url)
         print(f"Reloaded at {pd.Timestamp.now()}: Status Code {response.status_code}")
@@ -50,4 +98,9 @@ def set_interval(func, sec):
 set_interval(reload_website, 30)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
+
+# Additional code for testing
+data = pd.read_csv('Frnds4.csv', encoding='cp1252')
+Generator = data.iloc[[random.randint(1, 236)], [1, 2, 3, 4]]
+print(Generator)
